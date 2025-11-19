bcf177c26cc8991f77d01106909b8f8eb1cb62ba
subhash-kumar-nigam <nigamsubhash439@gmail.com>
2025-11-19 20:57:49 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
-import Contact from './pages/contact/Contact';
-import Footer from './components/Footer';
 import './App.css';
 import Login from './pages/user/Login';
 import Regsiter from './pages/user/Regsiter';
-import About from './pages/about/About';
 import UserDashboard from './pages/user/UserDashboard';
 import ScrollToTop from './pages/TopToBack/ScrollToTop';
 import Top from './pages/TopToBack/Top';
@@ -14,27 +11,20 @@ import OtpVerification from './pages/user/OtpVerification';
 import MyProfile from './pages/user/Myrpofile';
 import MyCourse from './pages/user/MyCourse';
 import MyJob from './pages/user/MyJob';
-import Header from './components/Header';
-import Projects from './pages/project/Projects.jsx';
-import Skills from './pages/skill/Skills.jsx';
-import ExperienceEducation from './pages/experience/ExperienceEducation.jsx';
+
+
 
 
 const App = () => {
   return (
     <>
-    <Header />
+    {/* <Header /> */}
     <ScrollToTop />
         <Routes>
         {/* General Pages */}
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/skills" element={<Skills/>} />
-        <Route path="/projects" element={<Projects/>} />
-        <Route path="/experience" element={<ExperienceEducation/>} />
-        
-         {/* Auth */}
+       
+        {/* Auth */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Regsiter />} />
         <Route path="/otpverify" element={<OtpVerification />} />
@@ -48,7 +38,7 @@ const App = () => {
 
       </Routes>
         <Top />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
